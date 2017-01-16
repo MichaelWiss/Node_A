0750007b8de85d739b321ed73e6d995e2853a6f8
MichaelWiss <michael.wiss@gmail.com>
2017-01-16 11:21:53 -0600

    requiring

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("starting app.js");
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+
+var user = os.userInfo();
+
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`);
