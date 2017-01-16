2f7292ffe2bf76dc8ea5a609ff35f3b24a4499b4
MichaelWiss <michael.wiss@gmail.com>
2017-01-16 14:29:47 -0600

    nodemon

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 console.log("starting app.js");
 
 const fs = require('fs');
-const os = require('os');
 const _= require('lodash');
+
 const notes = require('./notes.js');
 
 
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-var filteredArray = _.uniq(['Mike', 1, 'Mike', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// console.log('Result:', notes.add(9, -2));
-
-// var res = notes.addNote();
-// console.log(res);
-// var user = os.userInfo();
-
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+ 
