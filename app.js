86502f4d974f8f4c15e668e5674d2f9502d42d0e
MichaelWiss <michael.wiss@gmail.com>
2017-01-16 12:54:14 -0600

    lodash

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ console.log("starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const _= require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+// console.log('Result:', notes.add(9, -2));
 
 // var res = notes.addNote();
 // console.log(res);
