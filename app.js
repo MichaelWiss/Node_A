2352ebfce6d250524b7ba35057f5fd6f3e8427bf
MichaelWiss <michael.wiss@gmail.com>
2017-01-16 15:59:30 -0600

    yargs

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ console.log("starting app.js");
 
 const fs = require('fs');
 const _= require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
-console.log(process.argv);
+console.log('Process',process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
 	console.log('Listing all notes');
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,7 +1,9 @@
 console.log('starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+var addNote = () => {
+
 };
 
+module.exports = {
+	addNote
+};
