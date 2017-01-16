e2c4e3c6bd702547e79ee4ae982e45f084b12389
MichaelWiss <michael.wiss@gmail.com>
2017-01-16 12:19:06 -0600

    module exports

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
+console.log('Result:', notes.add(9, -2));
 
-var user = os.userInfo();
+// var res = notes.addNote();
+// console.log(res);
+// var user = os.userInfo();
 
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
