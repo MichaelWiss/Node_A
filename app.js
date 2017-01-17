e6f2e8f87dd9ad7917e778233b83f34e1bdce5d4
MichaelWiss <michael.wiss@gmail.com>
2017-01-17 09:40:15 -0600

    yargs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
 
 console.log('Yargs', argv);
@@ -19,7 +19,7 @@ if (command === 'add') {
 } else if (command === 'read') {
 	notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing note');
+    notes.removeNote(argv.title)
 } else {
 	console.log('Command not recognized');
 }
