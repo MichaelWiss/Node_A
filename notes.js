503b8cc101f51f0f54a800c7602bc9412e282c3a
MichaelWiss <michael.wiss@gmail.com>
2017-01-17 08:02:48 -0600

    notes

--- notes.js
+++ notes.js
@@ -1,9 +1,14 @@
 console.log('starting notes.js');
 
-var addNote = () => {
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
+};
 
+var getAll = () => {
+	console.log('Getting all notes');
 };
 
 module.exports = {
-	addNote
+	addNote,
+	getAll
 };
