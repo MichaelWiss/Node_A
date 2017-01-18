7c5479098d8f7cb850f422621ebe4cfb9d77ddff
MichaelWiss <michael.wiss@gmail.com>
2017-01-17 18:29:19 -0600

    check for duplicates

--- notes.js
+++ notes.js
@@ -16,12 +16,12 @@ var addNote = (title, body) => {
 
 	}
 
-    var duplicateNotes = notes.filter(() => {
-    	return note.title === title;
-    });
-
-	notes.push(note);
-	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    var duplicateNotes = notes.filter((note) => note.title === title);
+  
+    if (duplicateNotes.length === 0) {
+    	notes.push(note);
+	    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
